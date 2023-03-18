c30ae4dbb603999b541117e2f9d92645b976bc5a
gogynogy <usernamesgo@gmail.com>
2023-03-18 10:45:10 +0530

    correct admin message

--- messages/start_message.py
+++ messages/start_message.py
@@ -51,9 +51,11 @@ def get_text_admin():
            f'------------------------------------------------\n\n' \
            f'USDT к LKR                    {round(course_LKR)}\n' \
            f'USDT к LKR - 1%           {round(get_percent(course_LKR, 1))}\n' \
+           f'USDT к LKR - 3%           {round(get_percent(course_LKR, 3))}\n' \
            f'RUB к USDT                   {round(course_RUB, 2)}\n' \
            f'RUB к LKR                      {round(curency, 2)}\n' \
-           f'RUB к LKR - 1%              {round(get_percent(course_LKR, 1)/course_RUB, 2)}'
+           f'RUB к LKR - 1%              {round(get_percent(course_LKR, 1)/course_RUB, 2)}' \
+           f'RUB к LKR - 3%              {round(get_percent(course_LKR, 3)/course_RUB, 2)}'
     return text
 
 
