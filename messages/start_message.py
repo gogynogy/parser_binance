cedcbeafdf1edd59f75a872004bb9cd4d54dbd08
gogynogy <usernamesgo@gmail.com>
2023-07-25 16:16:24 +0530

    change currency

--- messages/start_message.py
+++ messages/start_message.py
@@ -15,7 +15,6 @@ def course_LKR_USDT(minu):
     bank_SRI = ['BANK']
     LKR_USDT = CrossratesGetter('LKR', 'USDT', "sell", bank_SRI)
     course_LKR = int(mean(LKR_USDT.give_list()))
-    return course_LKR - minu
 
 def course_VND_USDT(minu):
     bank_SRI = ['BANK']
@@ -39,14 +38,18 @@ def get_text_admin():
            f'200 000{probel}{get_percent(curency, 6)}{probel}{int(round(200000 / get_percent(curency, 6), -2))}\n\n' \
            f'400 000{probel}{get_percent(curency, 5)}{probel}{int(round(400000 / get_percent(curency, 5), -2))}\n\n' \
            f'USDT к LKR                    {round(course_LKR)}\n' \
-           f'USDT к LKR - 1%           {round(get_percent(course_LKR, 1))}\n' \
+           f'USDT к LKR - 1%           {round(get_percent(course_LKR, 1), 1)}\n' \
+           f'USDT к LKR - 2%           {round(get_percent(course_LKR, 2), 1)}\n' \
            f'USDT к LKR - 3%           {round(get_percent(course_LKR, 3), 1)}\n' \
-           f'USDT к LKR - 6%           {round(get_percent(course_LKR, 6))}\n' \
+           f'USDT к LKR - 4%           {round(get_percent(course_LKR, 4), 1)}\n' \
+           f'USDT к LKR - 6%           {round(get_percent(course_LKR, 6), 1)}\n' \
            f'USDT к LKR - 15            {round(course_LKR) - 15}\n\n' \
            f'RUB к USDT                   {round(course_RUB, 2)}\n\n' \
            f'RUB к LKR                      {round(curency, 2)}\n' \
            f'RUB к LKR - 1%              {round(get_percent(course_LKR, 1)/course_RUB, 2)}\n' \
-           f'RUB к LKR - 3%              {round(get_percent(course_LKR, 3)/course_RUB, 3)}'
+           f'RUB к LKR - 2%              {round(get_percent(course_LKR, 2) / course_RUB, 2)}\n' \
+           f'RUB к LKR - 3%              {round(get_percent(course_LKR, 3)/course_RUB, 2)}\n' \
+           f'RUB к LKR - 4%              {round(get_percent(course_LKR, 4) / course_RUB, 2)}\n'
     return text
 
 def get_text_admin_viet():
@@ -88,46 +91,19 @@ def get_start_text():
     course_RUB = give_rus_course(mean(RUB_USDT.give_list()))
     curency = course_LKR / course_RUB
     probel = '         |       '
-    text = f'Расчет курсов обмена рублей на юге Шри Ланки. \nДата актуализации: <b>{give_time()}</b>\n\n' \
-        f'<b>В Канди и Аругамбей</b> доступен самовывоз в центре по фикс курсу на сумму от 80 000 рупий\n' \
-        f'Сумма LKR         Курс            Сумма RUB\n' \
-        f'---------------------------------------\n' \
-        f'80 000 {probel}{get_percent(curency, 6)}{probel}{int(round(80000 / get_percent(curency, 6), -2))}\n' \
-        f'---------------------------------------\n' \
+    text = f'Расчет курсов обмена рублей на Шри Ланке. \nДата актуализации: <b>{give_time()}</b>\n\n' \
+        f'<b>В Канди и Аругамбей</b> доступен самовывоз в центре по фикс курсу {get_percent(curency, 6)}\n\n' \
+        f'Сумма LKR         Курс            Сумма RUB\n\n' \
+        f'80 000 {probel}{get_percent(curency, 6)}{probel}{int(round(80000 / get_percent(curency, 6), -2))}\n\n' \
+        f'<b>Мирисса / Велигама / Ахангама / Матара / Когала / Унаватуна / Галле</b>\n\n' \
+        f'100 000 {probel}{get_percent(curency, 6)}{probel}{int(round(100000 / get_percent(curency, 6), -2))}\n\n' \
+        f'400 000{probel}{get_percent(curency, 5)}{probel}{int(round(400000 / get_percent(curency, 5), -2))}\n\n' \
+        f'<b>В Коломбо</b> (Доставка в центр города от 200 000 рупий)\n\n' \
+        f'100 000{probel}{get_percent(curency, 7)}{probel}{int(round(100000 / get_percent(curency, 7), -2))}\n\n' \
         f'200 000{probel}{get_percent(curency, 6)}{probel}{int(round(200000 / get_percent(curency, 6), -2))}\n\n' \
-        f'<b>Мирисса / Велигама / Ахангама / Матара / Когала / Унаватуна / Галле</b>\n' \
-        f'Сумма LKR         Курс            Сумма RUB\n' \
-        f'---------------------------------------\n' \
-        f'50 000 {probel}{get_percent(curency, 8)}{probel}{int(round(50000 / get_percent(curency, 8), -2))}\n' \
-        f'---------------------------------------\n' \
-        f'100 000{probel}{get_percent(curency, 7)}{probel}{int(round(100000 / get_percent(curency, 7), -2))}\n' \
-        f'---------------------------------------\n' \
-        f'200 000{probel}{get_percent(curency, 6)}{probel}{int(round(200000 / get_percent(curency, 6), -2))}\n' \
-        f'---------------------------------------\n' \
-        f'400 000{probel}{get_percent(curency, 5)}{probel}{int(round(400000 / get_percent(curency, 5), -2))}\n' \
-        f'---------------------------------------\n' \
-        f'<b>В Мириссе</b>, если сами доедите до точки выдачи, курс будет минимальный {get_percent(curency, 6)} ' \
-        f'на любую сумму, дальше по сеткe.\n\n' \
-        f'<b>В Коломбо</b> (Доставка в центр города от 200 000 рупий)\n' \
-        f'Сумма LKR         Курс            Сумма RUB\n' \
-        f'---------------------------------------\n' \
-        f'100 000{probel}{get_percent(curency, 7)}{probel}{int(round(100000 / get_percent(curency, 7), -2))}\n' \
-        f'---------------------------------------\n' \
-        f'200 000{probel}{get_percent(curency, 6)}{probel}{int(round(200000 / get_percent(curency, 6), -2))}\n' \
-        f'---------------------------------------\n\n' \
-        f'<b>В Коломбо</b> (Самовывоз в центре города любой суммы)\n' \
-        f'Сумма LKR         Курс            Сумма RUB\n' \
-        f'---------------------------------------\n' \
-        f'100 000  {probel}{get_percent(curency, 6)}{probel}{int(round(100000 / get_percent(curency, 6), -2))}\n\n' \
-        f'<b>Аэропорт Негомбо</b> (Доставка от 300 000 рупий)\n' \
-        f'Сумма LKR         Курс            Сумма RUB\n' \
-        f'---------------------------------------\n' \
-        f'300 000{probel}{get_percent(curency, 8)}{probel}{int(round(300000 / get_percent(curency, 8), -2))}\n' \
-        f'---------------------------------------\n' \
-        f'400 000{probel}{get_percent(curency, 7)}{probel}{int(round(400000 / get_percent(curency, 7), -2))}\n' \
-        f'---------------------------------------\n' \
-        f'500 000{probel}{get_percent(curency, 6)}{probel}{int(round(500000 / get_percent(curency, 6), -2))}\n' \
-        f'---------------------------------------\n'
+        f'<b>Аэропорт Негомбо</b> (Доставка от 300 000 рупий)\n\n' \
+        f'300 000{probel}{get_percent(curency, 7)}{probel}{int(round(300000 / get_percent(curency, 7), -2))}\n\n' \
+        f'500 000{probel}{get_percent(curency, 6)}{probel}{int(round(500000 / get_percent(curency, 6), -2))}'
     return text
 
 
